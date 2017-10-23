04c821e7ce0351c2a8a4c2b64031ef572639ceed
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-10-23 23:02:46 +1100

    Refactor code + add specs

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
